b02f78563cc41e25955618cb7d844e7b1abd6f1f
xVermillionx <xVermillionx@notvalid>
2023-11-18 01:54:09 +0100

    Add semaphore and shm to rot8cpp

--- src/rot8cpp.cpp
+++ src/rot8cpp.cpp
@@ -11,7 +11,16 @@
 #include <csignal>
 #include <cassert>
 #include <cstring>
+#include <mutex>
 
+#include <fcntl.h>
+#include <semaphore.h>
+
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <unistd.h>
+const char* semaphoreName = "/rot8cpp_semaphore";
+const char* sharedMemoryName = "/rot8cpp_shared_memory";
 // Debug
 
 #ifdef DEBUG
@@ -34,21 +43,33 @@ bool isPiped (FILE* fd){
   return isatty(fileno(fd)) == 1;
 }
 
-struct {
+struct Global {
   bool running;
   uint8_t index;
   bool istty;
   bool debug;
-} g = { true, 0, false, false };
+} Global;
+struct Global init_g = { true, 0, false, false };
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 void sig_rtmin_handler(int signum, siginfo_t *siginfo, void *context)
 {
+  int shm_fd = shm_open(sharedMemoryName, O_RDWR, 0644);
+  struct Global* g = (struct Global*)mmap(NULL, sizeof(struct Global), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  if (g == MAP_FAILED) {
+    perror("mmap");
+    close(shm_fd);
+    return;
+  }
+
   if(siginfo->si_value.sival_int == 0){
-    g.running = !g.running;
+    g->running = !g->running;
     DEBUG_LOG("RTMIN Recieved\n");
   }
+
+  munmap(g, sizeof(struct Global));
+  close(shm_fd);
   /* switch (siginfo -> si_value.sival_int) {
     case:
     default:
@@ -59,13 +80,44 @@ void sig_rtmin_handler(int signum, siginfo_t *siginfo, void *context)
 
 
 void signalHandler(int signum) {
+  int shm_fd = shm_open(sharedMemoryName, O_WRONLY, 0644);
+  struct Global* g = (struct Global*)mmap(NULL, sizeof(struct Global), PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  if (g == MAP_FAILED) {
+    perror("mmap");
+    close(shm_fd);
+    return;
+  }
+
   if(signum == SIGUSR1){
-    g.running = false;
+    g->running = false;
     DEBUG_LOG("Stop Signal\n");
   }
   else if (signum == SIGUSR2) {
-    g.running = true;
+    g->running = true;
     DEBUG_LOG("Start Signal\n");
+  }
+  else {
+
+  }
+  munmap(g, sizeof(struct Global));
+  close(shm_fd);
+}
+
+
+sem_t* semaphore;
+std::mutex sharedMemoryMutex;
+int shm_fd;
+void cleanUp(int signum) {
+  if(signum == SIGINT){
+    sem_close(semaphore);
+    sem_unlink(semaphoreName);
+
+    // munmap(g, sizeof(struct Global));
+    close(shm_fd);
+    shm_unlink(sharedMemoryName);
+    // std::cerr << "Cleanup complete. Exiting." << std::endl;
+    // DEBUG_LOG("SIGINT\n");
+    exit(EXIT_FAILURE);
   }
   else {
 
@@ -168,10 +220,10 @@ void getRotDevices(std::vector<Device>& dev){
   });
 }
 
-void getPosition(Position& p, std::vector<Device> dev ) {
+void getPosition(Position& p, std::vector<Device> dev, struct Global *g) {
   uint8_t i = 0;
   float *pos = reinterpret_cast<float*>(&p);
-  for (const auto& d : dev[g.index].devs) {
+  for (const auto& d : dev[g->index].devs) {
     // std::cout << "Getting Position of: " << dev[g.index].name << std::endl;
     std::ifstream s(d);
     s >> pos[i];
@@ -184,20 +236,39 @@ void getPosition(Position& p, std::vector<Device> dev ) {
 
 void getPosT (Position& p){
 
+  int shm_fd = shm_open(sharedMemoryName, O_RDONLY, 0644);
+  struct Global* g = (struct Global*)mmap(NULL, sizeof(struct Global), PROT_READ, MAP_SHARED, shm_fd, 0);
+  if (g == MAP_FAILED) {
+    perror("mmap");
+    close(shm_fd);
+    return;
+  }
+
   std::vector<Device> dev;
 
   getRotDevices(dev);
 
   while(true){
     // Only Run if allowed
-    if(g.running) {
-      getPosition(p, dev);
+    {
+      std::lock_guard<std::mutex> lock(sharedMemoryMutex);
+      if(g->running) {
+        getPosition(p, dev, g);
+      }
     }
     std::this_thread::sleep_for(std::chrono::milliseconds(POS_POLL_TIME));
   }
 }
 
 void printPosT (Position& p){
+
+  int shm_fd = shm_open(sharedMemoryName, O_RDONLY, 0644);
+  struct Global* g = (struct Global*)mmap(NULL, sizeof(struct Global), PROT_READ, MAP_SHARED, shm_fd, 0);
+  if (g == MAP_FAILED) {
+    perror("mmap");
+    close(shm_fd);
+    return;
+  }
 
   Orientation current = { NORMAL_0, p };
 
@@ -211,23 +282,26 @@ void printPosT (Position& p){
 
   while(true){
     // Only Run if allowed
-    if(g.running) {
+    {
+      std::lock_guard<std::mutex> lock(sharedMemoryMutex);
+    if(g->running) {
       float pseudox = std::round(current.p.x);
       float pseudoy = std::round(current.p.y);
       // if (pseudox != invalidx || pseudoy != invalidy) rotatetdir = 90 * pseudox + (current.p.y > 0.8 ? 180 : 0);
       if (pseudox+pseudoy != 0 || pseudoy+pseudox != 2) rotatetdir = static_cast<int>(90 * pseudox + (current.p.y > 0.8 ? 180 : 0));
 #ifdef DEBUG
-      if (g.debug){
-        if (g.istty) DEBUG_LOG(pseudox == invalidx ? RED : NONE);
+      if (g->debug){
+        if (g->istty) DEBUG_LOG(pseudox == invalidx ? RED : NONE);
         DEBUG_LOG(current.p.x);
-        if (g.istty) DEBUG_LOG(NONE);
+        if (g->istty) DEBUG_LOG(NONE);
         DEBUG_LOG(", ");
-        if (g.istty) DEBUG_LOG(pseudoy == invalidy ? RED : NONE);
+        if (g->istty) DEBUG_LOG(pseudoy == invalidy ? RED : NONE);
         DEBUG_LOG(current.p.y);
-        if (g.istty) DEBUG_LOG(NONE);
+        if (g->istty) DEBUG_LOG(NONE);
         DEBUG_LOG(", ", current.p.z, "\n");
       }
 #endif // DEBUG
+    }
 
       if (rotatetdir != oldrotatetdir) std::cout << rotatetdir << std::endl;
       oldrotatetdir = rotatetdir;
@@ -263,61 +337,128 @@ void help(){
 
 int main (int argc, char* argv[]) {
 
-  g.istty = isPiped(stdout);
+  // Semaphore
+  semaphore = sem_open(semaphoreName, O_CREAT | O_EXCL, 0644, 1);
+  const bool realProg = (semaphore != SEM_FAILED);
+
+  // Shared Memory
+  shm_fd = shm_open(sharedMemoryName, O_CREAT | O_RDWR, 0644);
+  ftruncate(shm_fd, sizeof(struct Global));
+  struct Global* g = (struct Global*)mmap(NULL, sizeof(struct Global), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+  if (g == MAP_FAILED) {
+    perror("mmap");
+    close(shm_fd);
+    return EXIT_FAILURE;
+  }
+
+  if(realProg) *g = init_g;
+
+  if(realProg) g->istty = isPiped(stdout);
 
   for(int i = 1; i < argc; i++){
-    if(strcmp(argv[i], "--devIndex") == 0){
-      assert(strlen(argv[i]) > 3);
-      unsigned int x = static_cast<unsigned int>(atoi(argv[i]));
-      assert(x < U8_MAX+1);
-      i++;
-      g.index = static_cast<uint8_t>(x);
-    }
-    else if(strcmp(argv[i], "--listDevIndex") == 0){
-      std::vector<Device> dev;
-      getRotDevices(dev);
-      int k = 0;
-      for(auto & d : dev){
-        std::cout << k++ << ": " << d.name << std::endl;
+    if(realProg){
+      if(strcmp(argv[i], "--devIndex") == 0){
+        assert(strlen(argv[i]) > 3);
+        unsigned int x = static_cast<unsigned int>(atoi(argv[i]));
+        assert(x < U8_MAX+1);
+        i++;
+        g->index = static_cast<uint8_t>(x);
       }
-      return 0;
-    }
+      else if(strcmp(argv[i], "--listDevIndex") == 0){
+        std::vector<Device> dev;
+        getRotDevices(dev);
+        int k = 0;
+        for(auto & d : dev){
+          std::cout << k++ << ": " << d.name << std::endl;
+        }
+        return 0;
+      }
 #ifdef DEBUG
-    else if(strcmp(argv[i], "--debug") == 0){
-      g.debug = true;
-    }
+      else if(strcmp(argv[i], "--debug") == 0){
+        g->debug = true;
+      }
 #endif // DEBUG
-    else if(strcmp(argv[i], "--paused") == 0){
-      g.running = false;
+      else if(strcmp(argv[i], "--paused") == 0){
+        g->running = false;
+      }
+      else if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0){
+        help();
+        munmap(g, sizeof(struct Global));
+        close(shm_fd);
+        shm_unlink(sharedMemoryName);
+        sem_unlink(semaphoreName);
+        return 0;
+      }
+      else{
+        help();
+        munmap(g, sizeof(struct Global));
+        close(shm_fd);
+        shm_unlink(sharedMemoryName);
+        sem_unlink(semaphoreName);
+        return 1;
+      }
     }
-    else if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0){
-      help();
-      return 0;
-    }
-    else{
-      help();
-      return 1;
+    else {
+      if(strcmp(argv[i], "--cleanup") == 0){
+        munmap(g, sizeof(struct Global));
+        close(shm_fd);
+        shm_unlink(sharedMemoryName);
+        sem_unlink(semaphoreName);
+        std::cerr << "Unlinked Semaphore" << std::endl;
+        return 0;
+      }
+      else if(strcmp(argv[i], "--state") == 0){
+        if(isPiped(stdout)) {
+          std::cout << "State:";
+        }
+        std::cout << g->running << std::endl;
+        munmap(g, sizeof(struct Global));
+        close(shm_fd);
+        // shm_unlink(sharedMemoryName);
+        // sem_unlink(semaphoreName);
+        return 0;
+      }
+      else{
+        help();
+        munmap(g, sizeof(struct Global));
+        close(shm_fd);
+        shm_unlink(sharedMemoryName);
+        sem_unlink(semaphoreName);
+        return 1;
+      }
     }
   }
 
-  signal(SIGUSR1, signalHandler);
-  signal(SIGUSR2, signalHandler);
-  struct sigaction act;
-  memset (&act, 0, sizeof (act));
-  // set signal handler for SIGRTMIN
-  act.sa_sigaction = sig_rtmin_handler;
-  act.sa_flags = SA_SIGINFO;
-  if (sigaction (SIGRTMIN, &act, NULL) == -1)
-      std::cerr << "sigaction" << std::endl;
+  munmap(g, sizeof(struct Global));
+  close(shm_fd);
 
-  Position pos = {0, 0, 0};
+  if (realProg) {
+    signal(SIGINT, cleanUp);
+    signal(SIGUSR1, signalHandler);
+    signal(SIGUSR2, signalHandler);
+    struct sigaction act;
+    memset (&act, 0, sizeof (act));
+    // set signal handler for SIGRTMIN
+    act.sa_sigaction = sig_rtmin_handler;
+    act.sa_flags = SA_SIGINFO;
+    if (sigaction (SIGRTMIN, &act, NULL) == -1)
+        std::cerr << "sigaction" << std::endl;
 
-  std::thread t1(getPosT, std::ref(pos));
-  std::thread t2(printPosT, std::ref(pos));
+    Position pos = {0, 0, 0};
 
-  t1.join();
-  t2.join();
+    std::thread t1(getPosT, std::ref(pos));
+    std::thread t2(printPosT, std::ref(pos));
 
+    t1.join();
+    t2.join();
+
+    sem_close(semaphore);
+    sem_unlink(semaphoreName);
+  } else {
+    std::cerr << "Another instance is already running." << std::endl;
+  }
+
+  if (realProg) shm_unlink(sharedMemoryName);
   return 0;
 
 }
