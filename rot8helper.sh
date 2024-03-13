44e1d9c66829472d7d82ebb684fef98fadada9f5
xVermillionx <xVermillionx@notvalid>
2024-03-13 23:25:14 +0100

    Update rot8helper

--- rot8helper.sh
+++ rot8helper.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-wm="$(echo $XDG_CURRENT_DESKTOP | cut -f1 -d:)"
+wm="$(echo ${XDG_CURRENT_DESKTOP:-$TERM} | cut -f1 -d:)"
 monitor0='eDP-1'
 
 # Default
@@ -11,7 +11,8 @@ rot8val=(normal 90 270 180)
 case $wm in
   hyprland|Hyprland)
     cmd="hyprctl keyword monitor $monitor0,transform,"
-    cmd2="hyprctl keyword input:touchdevice:transform "
+    # cmd2="hyprctl keyword input:touchdevice:transform "
+    cmd2="hyprctl keyword device:wacom-hid-49c3-finger:transform "
     rot8val=(0 1 3 2)
     ;;
   river)
@@ -19,6 +20,11 @@ case $wm in
   sway)
     cmd="swaymsg output * transform "
     # rot8val=(normal 90 270 180)
+    ;;
+  linux)
+    sudo -v || exit 1
+    cmd='sudo tee /sys/class/graphics/fbcon/rotate_all <<< '
+    rot8val=(0 1 3 2)
     ;;
   *)
     cmd="wlr-randr --output --transform "
@@ -30,20 +36,24 @@ while read rot;
 do
   case $rot in
     0)
-      ${cmd}${rot8val[0]} >/dev/null
-      ${cmd2:+${cmd2}${rot8val[0]} >dev/null}
+      ${cmd}${rot8val[0]}
+      # ${cmd}${rot8val[0]} >/dev/null
+      ${cmd2:+${cmd2}${rot8val[0]}} >/dev/null
       ;;
     90)
-      ${cmd}${rot8val[1]} >/dev/null
-      ${cmd2:+${cmd2}${rot8val[1]} >dev/null}
+      ${cmd}${rot8val[1]}
+      # ${cmd}${rot8val[1]}
+      ${cmd2:+${cmd2}${rot8val[1]}} >/dev/null
       ;;
     -90)
-      ${cmd}${rot8val[2]} >/dev/null
-      ${cmd2:+${cmd2}${rot8val[2]} >dev/null}
+      ${cmd}${rot8val[2]}
+      # ${cmd}${rot8val[2]}
+      ${cmd2:+${cmd2}${rot8val[2]}} >/dev/null
       ;;
     180)
-      ${cmd}${rot8val[3]} >/dev/null
-      ${cmd2:+${cmd2}${rot8val[3]} >dev/null}
+      ${cmd}${rot8val[3]}
+      # ${cmd}${rot8val[3]} >/dev/null
+      ${cmd2:+${cmd2}${rot8val[3]}} >/dev/null
       ;;
     *)
       echo ERROR
